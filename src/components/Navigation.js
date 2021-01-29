60d535b8eb4c917e5fec76a7e7ba9953f3df9aa3
lukasz protosawicki <lukas.es.p@o2.pl>
2021-01-29 16:43:05 +0100

    add components scss

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,19 @@
 function Navigation() {
   return (
     <div className="navigation">
-      <ul>
-        <li>
-          <a href="default.asp">About me</a>
-        </li>
-        <li>
-          <a href="news.asp">Projects</a>
-        </li>
-        <li>
-          <a href="contact.asp">Contact</a>
-        </li>
-      </ul>
+      <div className="content-navigation">
+        <ul>
+          <li>
+            <a href="default.asp">About me</a>
+          </li>
+          <li>
+            <a href="news.asp">Projects</a>
+          </li>
+          <li>
+            <a href="contact.asp">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
