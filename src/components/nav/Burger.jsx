c14a2e042f262139b8a20637a9c3d616877580ca
lukasz protosawicki <lukas.es.p@o2.pl>
2021-02-01 18:34:39 +0100

    add state open

--- src/components/nav/Burger.jsx
+++ src/components/nav/Burger.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Burger = () => {
+
+    const [open, setOpen] = useState(false)
     return (
         <div>
-            <div className="button">
-             <div className="button-nav"></div>
+            <div className="button" open={open} onClick={() => setOpen(!open)}>
+              <div className="button-nav"></div>
+              <div className="button-nav"></div>
+              <div className="button-nav"></div>
           </div>
         </div>
             )
