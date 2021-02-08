9cc1377c60c749c5a38e5ef7726ff021d581b4e7
lukasz protosawicki <lukas.es.p@o2.pl>
2021-02-08 16:59:50 +0100

    clear styles mixed

--- src/components/Textprojects.js
+++ src/components/Textprojects.js
@@ -7,15 +7,33 @@ function Textprojects() {
         <a href="https://lukaszprotosawicki.github.io/">
           Project Lukasz Protosawicki
         </a>
-        <a href="stoper-firebase/">Project Stoper Firebase</a>
-        <a href="project-clime/index.html">Project Main</a>
-        <a href="project-shop/index.html">Project Shop </a>
-        <a href="project-gallery/index.html">Project Gallery</a>
-        <a href="project-stomatolog/">Project Stomatolog</a>
-        <a href="project-akordeon/">Project Akordeon</a>
-        <a href="project-calculator/">Project Calculator</a>
-        <a href="project-calculator-damage/">Project Calculator Damage</a>
-        <a href="project4-tic-tac-toe/">Project Tic Tac Toe</a>
+        <a href="https://lukaszprotosawicki.github.io/my-projects/stoper-firebase/">
+          Project Stoper Firebase
+        </a>
+        <a href="https://lukaszprotosawicki.github.io/my-projects/project-clime/index.html">
+          Project Main
+        </a>
+        <a href="https://lukaszprotosawicki.github.io/my-projects/project-shop/index.html">
+          Project Shop{" "}
+        </a>
+        <a href="https://lukaszprotosawicki.github.io/my-projects/project-gallery/index.html">
+          Project Gallery
+        </a>
+        <a href="https://lukaszprotosawicki.github.io/my-projects/project-stomatolog/">
+          Project Stomatolog
+        </a>
+        <a href="https://lukaszprotosawicki.github.io/my-projects/project-akordeon/">
+          Project Akordeon
+        </a>
+        <a href="https://lukaszprotosawicki.github.io/my-projects/project-calculator/">
+          Project Calculator
+        </a>
+        <a href="https://lukaszprotosawicki.github.io/my-projects/project-calculator-damage/">
+          Project Calculator Damage
+        </a>
+        <a href="https://lukaszprotosawicki.github.io/my-projects/project4-tic-tac-toe/">
+          Project Tic Tac Toe
+        </a>
       </div>
     </div>
   );
