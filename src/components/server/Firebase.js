da8bf0d641f67e9e8b4faac38c0eac1a3cb5038d
lukasz protosawicki <lukas.es.p@o2.pl>
2021-02-09 17:27:20 +0100

    install firebase, add folder server with all items.js

---
+++ src/components/server/Firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const Firebase = firebase.initializeApp({
+  apiKey: "AIzaSyDaHToxUecCiSS0UuOd0TH4WO8K1ipaPAM",
+  authDomain: "lukaszprotosawicki-4c4d5.firebaseapp.com",
+  projectId: "lukaszprotosawicki-4c4d5",
+  storageBucket: "lukaszprotosawicki-4c4d5.appspot.com",
+  messagingSenderId: "994050648302",
+  appId: "1:994050648302:web:41d6ef305ce83cf2d5ebff",
+});
+
+firebase.initializeApp(firebaseConfig);
